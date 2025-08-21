e6a219f92f87ff986de9420ba31b5b0e193c5b46
code-boy-raju <rraj1572527@gmail.com>
2025-08-21 20:21:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Calculator from './components/calculator.jsx'
+
+class App extends Component{
+  render(){
+    return(
+      <Calculator/>
+    )
+  }
+}
+export default App
---
+++ src/components/calculator.jsx
@@ -0,0 +1,63 @@
+import  { Component } from "react";
+import "../App.css"; 
+
+class Calculator extends Component {
+  state = {
+    input: "",
+    result: ""
+  };
+
+  handleClick = (value) => {
+    this.setState({ input: this.state.input + value });
+  };
+
+  clear = () => {
+    this.setState({ input: "", result: "" });
+  };
+
+  
+  backspace = () => {
+    this.setState({ input: this.state.input.slice(0, -1) });
+  };
+
+  calculate = () => {
+    try {
+      // evaluate expression safely
+      const res = eval(this.state.input); 
+      this.setState({ result: res });
+    } catch {
+      this.setState({ result: "Error" });
+    }
+  };
+
+  render() {
+    const buttons = [
+      "7","8","9","/",
+      "4","5","6","*",
+      "1","2","3","-",
+      "0",".","+"
+    ];
+
+    return (
+      <div className="calc-container">
+        <h1> Calculator</h1>
+        <div className="calc-display">
+          <div className="calc-input">{this.state.input || "0"}</div>
+          <div className="calc-result">{this.state.result}</div>
+        </div>
+        <div className="calc-buttons">
+          {buttons.map((btn, i) => (
+            <button key={i} onClick={() => this.handleClick(btn)}>
+              {btn}
+            </button>
+          ))}
+          <button className="equal" onClick={this.calculate}>=</button>
+          <button className="clear" onClick={this.clear}>C</button>
+          <button className="back" onClick={this.backspace}>⌫</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Calculator;
